4d7ae9827d920069eaa5b7c4208ef5c774d9810f
VinayMandhaan <vinaymandhaan29@gmail.com>
2022-03-23 19:51:08 +0500

    Search, Sort List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  
+  const [data, setData] = useState
+  ([
+    {
+      id:1,
+      title:'Mobile',
+      status:false,
+      priority:1
+    },
+    {
+      id:2,
+      title:'Work',
+      status:false,
+      priority:3
+    },
+    {
+      id:5,
+      title:'Video',
+      status:false,
+      priority:2
+    },
+    {
+      id:3,
+      title:'Task',
+      status:false,
+      priority:3
+    },
+    {
+      id:4,
+      title:'Web',
+      status:false,
+      priority:1
+    },
+  ])
+  const [filteredList, setFilteredList] = useState(data)
+  const checkPriority = (val) => { 
+    var priorityText
+    if(val === 1){
+      return priorityText = 'High'
+    } else if(val === 2){
+      return priorityText = 'Medium'
+    } else {
+      return priorityText = 'Low'
+    }
+  }
+
+  const onChangeValue = (id) => {
+    const newData = [...data]
+    newData.map(s => {
+      if(s.id === id){
+        console.log(s,'HEHE')
+        s.status = !s.status
+      }
+    })
+  setFilteredList(newData)
+
+  }
+
+  const searchTask = (e) => {
+    console.log(e.target.value)
+    var value = e.target.value
+    const filteredTasks = data.filter(val => val.title.toLowerCase().includes(e.target.value.toLowerCase()))
+    console.log(filteredTasks,'HEHEH')
+    setFilteredList(filteredTasks)
+  }
+
+  const swapTopElements = (a,b) => {
+    console.log(a,b,'SWAPAPP')
+    console.log('SWAP CALLED')
+    var tempData = [...data]
+    var temp = tempData[a]
+    tempData[a] = tempData[b]
+    tempData[b] = temp
+    setFilteredList(tempData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' onChange={(e)=>searchTask(e)}/>
+      {
+        filteredList.map((val, i) => (
+          <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+            <p>{val.id}</p>
+            <h2>{val.title}</h2>
+            <input type={"checkbox"} checked={val.status} onChange={()=>onChangeValue(val.id)} />
+            <p>{checkPriority(val.priority)}</p>
+            <p onClick={()=> i-1 >= 0 && swapTopElements(i,i-1)}>Top</p>
+            <p>Bottom</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
